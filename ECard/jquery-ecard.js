b37349d177fb000f495c28c45afcbc700dd5f60b
edwinfapp <edwinfapp@gmail.com>
2016-05-10 07:32:42 -0500

    Ajuste en Eventos

--- ECard/jquery-ecard.js
+++ ECard/jquery-ecard.js
@@ -72,197 +72,6 @@
 
 			this.$ecard.ajust();
 
-			// ---------------------------------------------------------------------
-
-			if (!isNew) {
-				return;
-			}
-
-			// ---------------------------------------------------------------------
-
-			$(this).bind('mousedown', function(e) {
-
-				if (this.$ecard.touching) {
-					return;
-				}
-
-				this.$ecard.touching = true;
-				this.$ecard.moving = false;
-				this.$ecard.initTime = new Date().getTime();
-				this.$ecard.endTime = null;
-				this.$ecard.dx = 0;
-
-				this.$ecard.initcards = parseInt(this.$ecard.ec_cards.css("left"));
-
-				this.$ecard.touch_identifier = 0;
-				this.$ecard.startPoint = {
-					x : e.pageX,
-					y : e.pageY
-				};
-
-			});
-
-			// ---------------------------------------------------------------------
-
-			$(this).bind('touchstart', function(e) {
-
-				if (this.$ecard.touching) {
-					return;
-				}
-
-				this.$ecard.touching = true;
-				this.$ecard.moving = false;
-				this.$ecard.initTime = new Date().getTime();
-				this.$ecard.endTime = null;
-				this.$ecard.dx = 0;
-
-				this.$ecard.initcards = parseInt(this.$ecard.ec_cards.css("left"));
-
-				if (e.originalEvent.touches && e.originalEvent.touches[0]) {
-
-					var touch = e.originalEvent.touches[0];
-
-					this.$ecard.touch_identifier = touch.identifier;
-					this.$ecard.startPoint = {
-						x : touch.screenX,
-						y : touch.screenY
-					};
-
-				}
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('mouseup', function(e) {
-				this.$ecard.endTime = new Date().getTime();
-
-				if (!this.$ecard.touching) {
-					return;
-				}
-
-				this.$ecard.ajust();
-				this.$ecard.touching = false;
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('touchend', function(e) {
-
-				this.$ecard.endTime = new Date().getTime();
-
-				if (!this.$ecard.touching) {
-					return;
-				}
-
-				for (var i = 0; i < e.originalEvent.changedTouches.length; i++) {
-
-					var touch = e.originalEvent.changedTouches[i];
-
-					if (touch.identifier == this.$ecard.touch_identifier) {
-						this.$ecard.ajust();
-					}
-				}
-
-				this.$ecard.touching = false;
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('mouseleave', function(e) {
-				if (this.$ecard.touching) {
-					e.preventDefault();
-					this.$ecard.touching = false;
-					this.$ecard.ajust();
-				}
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('touchcancel', function(e) {
-				if (this.$ecard.touching) {
-					e.preventDefault();
-					this.$ecard.touching = false;
-					this.$ecard.ajust();
-				}
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('mousemove', function(e) {
-
-				if (!this.$ecard.touching) {
-					return;
-				}
-
-				var dx = e.pageX - this.$ecard.startPoint.x;
-				var dy = e.pageY - this.$ecard.startPoint.y;
-
-				console.log(dx);
-
-				if (this.$ecard.moving) {
-					e.preventDefault();
-					this.$ecard.dx = dx;
-					var ll = this.$ecard.initcards + dx;
-					this.$ecard.ec_cards.css("left", ll + "px");
-				} else {
-
-					if (Math.abs(dy) > 15) {
-						this.$ecard.touching = false;
-						return false;
-					}
-
-					if (Math.abs(dx) > 30) {
-						this.$ecard.moving = true;
-						this.$ecard.ec_cards.addClass("ec_cards_hover");
-					}
-				}
-
-			});
-
-			// ----------------------------------------------------------------------
-
-			$(this).bind('touchmove', function(e) {
-
-				if (!this.$ecard.touching) {
-					return;
-				}
-
-				if (e.originalEvent.touches.length == 0 || this.$ecard.startPoint === null) {
-					this.$ecard.touching = false;
-					return false;
-				}
-
-				for (var i = 0; i < e.originalEvent.touches.length; i++) {
-
-					var touch = e.originalEvent.touches[i];
-
-					if (touch.identifier == this.$ecard.touch_identifier) {
-						var dx = touch.screenX - this.$ecard.startPoint.x;
-						var dy = touch.screenY - this.$ecard.startPoint.y;
-
-						if (this.$ecard.moving) {
-							e.preventDefault();
-							this.$ecard.dx = dx;
-							var ll = this.$ecard.initcards + dx;
-							this.$ecard.ec_cards.css("left", ll + "px");
-						} else {
-
-							if (Math.abs(dy) > 15) {
-								this.$ecard.touching = false;
-								return false;
-							}
-
-							if (Math.abs(dx) > 30) {
-								this.$ecard.moving = true;
-								this.$ecard.ec_cards.addClass("ec_cards_hover");
-							}
-						}
-
-					}
-				}
-
-			});
-
 			// ----------------------------------------------------------------------
 
 		});
@@ -286,4 +95,226 @@ $(function() {
 			}
 		});
 	});
+
+	// ---------------------------------------------------
+
+	$("body").on('mousedown', ".ec_area", function(e) {
+
+		console.log("mousedown");
+
+		if (this.$ecard === undefined) {
+			return;
+		}
+
+		if (this.$ecard.touching) {
+			return;
+		}
+
+		this.$ecard.touching = true;
+		this.$ecard.moving = false;
+		this.$ecard.initTime = new Date().getTime();
+		this.$ecard.endTime = null;
+		this.$ecard.dx = 0;
+
+		this.$ecard.initcards = parseInt(this.$ecard.ec_cards.css("left"));
+
+		this.$ecard.touch_identifier = 0;
+		this.$ecard.startPoint = {
+			x : e.pageX,
+			y : e.pageY
+		};
+
+	});
+
+	// ---------------------------------------------------------------------
+
+	$("body").bind('touchstart', function(e) {
+
+		if (this.$ecard === undefined) {
+			return;
+		}
+
+		if (this.$ecard.touching) {
+			return;
+		}
+
+		this.$ecard.touching = true;
+		this.$ecard.moving = false;
+		this.$ecard.initTime = new Date().getTime();
+		this.$ecard.endTime = null;
+		this.$ecard.dx = 0;
+
+		this.$ecard.initcards = parseInt(this.$ecard.ec_cards.css("left"));
+
+		if (e.originalEvent.touches && e.originalEvent.touches[0]) {
+
+			var touch = e.originalEvent.touches[0];
+
+			this.$ecard.touch_identifier = touch.identifier;
+			this.$ecard.startPoint = {
+				x : touch.screenX,
+				y : touch.screenY
+			};
+
+		}
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('mouseup', function(e) {
+
+		$(".ec_area").each(function() {
+
+			this.$ecard.endTime = new Date().getTime();
+
+			if (!this.$ecard.touching) {
+				return;
+			}
+
+			this.$ecard.ajust();
+			this.$ecard.touching = false;
+
+		});
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('touchend', function(e) {
+
+		$(".ec_area").each(function() {
+
+			this.$ecard.endTime = new Date().getTime();
+
+			if (!this.$ecard.touching) {
+				return;
+			}
+
+			for (var i = 0; i < e.originalEvent.changedTouches.length; i++) {
+
+				var touch = e.originalEvent.changedTouches[i];
+
+				if (touch.identifier == this.$ecard.touch_identifier) {
+					this.$ecard.ajust();
+				}
+			}
+
+			this.$ecard.touching = false;
+
+		});
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('mouseleave', function(e) {
+
+		$(".ec_area").each(function() {
+
+			if (this.$ecard.touching) {
+				e.preventDefault();
+				this.$ecard.touching = false;
+				this.$ecard.ajust();
+			}
+
+		});
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('touchcancel', function(e) {
+
+		$(".ec_area").each(function() {
+
+			if (this.$ecard.touching) {
+				e.preventDefault();
+				this.$ecard.touching = false;
+				this.$ecard.ajust();
+			}
+
+		});
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('mousemove', function(e) {
+
+		$(".ec_area").each(function() {
+
+			if (!this.$ecard.touching) {
+				return;
+			}
+
+			var dx = e.pageX - this.$ecard.startPoint.x;
+			var dy = e.pageY - this.$ecard.startPoint.y;
+
+			if (this.$ecard.moving) {
+				e.preventDefault();
+				this.$ecard.dx = dx;
+				var ll = this.$ecard.initcards + dx;
+				this.$ecard.ec_cards.css("left", ll + "px");
+			} else {
+
+				if (Math.abs(dy) > 15) {
+					this.$ecard.touching = false;
+					return false;
+				}
+
+				if (Math.abs(dx) > 30) {
+					this.$ecard.moving = true;
+					this.$ecard.ec_cards.addClass("ec_cards_hover");
+				}
+			}
+
+		});
+
+	});
+
+	// ----------------------------------------------------------------------
+
+	$("body").on('touchmove', function(e) {
+
+		$(".ec_area").each(function() {
+
+			if (!this.$ecard.touching) {
+				return;
+			}
+
+			if (e.originalEvent.touches.length == 0 || this.$ecard.startPoint === null) {
+				this.$ecard.touching = false;
+				return false;
+			}
+
+			for (var i = 0; i < e.originalEvent.touches.length; i++) {
+
+				var touch = e.originalEvent.touches[i];
+
+				if (touch.identifier == this.$ecard.touch_identifier) {
+					var dx = touch.screenX - this.$ecard.startPoint.x;
+					var dy = touch.screenY - this.$ecard.startPoint.y;
+
+					if (this.$ecard.moving) {
+						e.preventDefault();
+						this.$ecard.dx = dx;
+						var ll = this.$ecard.initcards + dx;
+						this.$ecard.ec_cards.css("left", ll + "px");
+					} else {
+
+						if (Math.abs(dy) > 15) {
+							this.$ecard.touching = false;
+							return false;
+						}
+
+						if (Math.abs(dx) > 30) {
+							this.$ecard.moving = true;
+							this.$ecard.ec_cards.addClass("ec_cards_hover");
+						}
+					}
+
+				}
+			}
+		});
+
+	});
+
+	// ----------------------------------------------------------------------
+
 });
